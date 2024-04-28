bb29e41221a24e02c948b224a218e8f62745fa0b
FatimalRamadan <2180004473@iau.edu.sa>
2024-04-28 09:54:10 +0300

    Inset payment

--- src/Database/Categories.sql
+++ src/Database/Categories.sql
@@ -4,10 +4,11 @@ CREATE TABLE Categories (
     description TEXT
 );
 
-INSERT INTO Categories (category_name, description) VALUES ('Dresses', 'Various types of dresses.');
-INNER INTO Categories (category_name, description) VALUES ('Tops', 'Shirts, Bodysuits, T-Shirts, Blouses, Sweaters');
-INSERT INTO Categories (category_name, description) VALUES ('Bottoms', 'Pants, Jeans, Teousers, Skirts, Shorts');
-INSERT INTO Categories (category_name, description) VALUES ('Jackets', 'Various types of jackets.');
-INSERT INTO Categories (category_name, description) VALUES ('Bags', 'Crossbody, Shoulder Bag, Handbags, Backpacks, Tote Bags');
-INSERT INTO Categories (category_name, description) VALUES ('Shoes', 'Flat Shoes, High-Heels, Sandals, Boots, Sneakers');
-INSERT INTO Categories (category_name, description) VALUES ('Accessories', 'Scarves, hats, gloves, Belts, Jewellery');
+INSERT INTO Categories (category_name, description) 
+ VALUES ('Dresses', 'Various types of dresses.'),
+        ('Tops', 'Shirts, Bodysuits, T-Shirts, Blouses, Sweaters'),
+        ('Bottoms', 'Pants, Jeans, Teousers, Skirts, Shorts'),
+        ('Jackets', 'Various types of jackets.'),
+        ('Bags', 'Crossbody, Shoulder Bag, Handbags, Backpacks, Tote Bags'),
+        ('Shoes', 'Flat Shoes, High-Heels, Sandals, Boots, Sneakers'),
+        ('Accessories', 'Scarves, hats, gloves, Belts, Jewellery');
