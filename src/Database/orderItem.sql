ab025cda2eeffb8c08bfd1a52283c79272406d61
Atheer Alsaedii <atheeralsaedi.99@gmail.com>
2024-04-28 11:09:58 +0300

    inssert for order-item

--- src/Database/orderItem.sql
+++ src/Database/orderItem.sql
@@ -1,9 +1,19 @@
 CREATE TABLE Order_Item(
 order_item_id SERIAL PRIMARY KEY,
-price DECIMAL(10,2) NOT NULL,
+price VARCHAR(50) NOT NULL,
 quantity INT NOT NULL,
 order_id SERIAL,
 product_id SERIAL,
 FOREIGN KEY (product_id) REFERENCES Proudects(product_id) ,
 FOREIGN KEY (order_id) REFERENCES Orders (order_id)
 );
+
+INSERT INTO Order_Item(price, quantity, order_id, product_id) 
+VALUES 
+('1,303 SAR', 1, 1, 1), 
+('118 SAR', 2, 2, 2),
+('157 SAR', 1, 3, 3), 
+('589 SAR', 1, 4,4),
+('516 SAR', 3, 5, 5), 
+('540 SAR', 2, 6, 6),
+('106 SAR', 2, 7, 7);
