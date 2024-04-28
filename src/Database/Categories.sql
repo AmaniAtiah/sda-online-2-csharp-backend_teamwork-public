9b7ac477071f1227306a1d7b330de7317300215a
FatimalRamadan <2180004473@iau.edu.sa>
2024-04-28 09:42:37 +0300

    Insert clothing categories

--- src/Database/Categories.sql
+++ src/Database/Categories.sql
@@ -3,3 +3,11 @@ CREATE TABLE Categories (
     category_name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT
 );
+
+INSERT INTO Categories (category_name, description) VALUES ('Dresses', 'Various types of dresses.');
+INNER INTO Categories (category_name, description) VALUES ('Tops', 'Shirts, Bodysuits, T-Shirts, Blouses, Sweaters');
+INSERT INTO Categories (category_name, description) VALUES ('Bottoms', 'Pants, Jeans, Teousers, Skirts, Shorts');
+INSERT INTO Categories (category_name, description) VALUES ('Jackets', 'Various types of jackets.');
+INSERT INTO Categories (category_name, description) VALUES ('Bags', 'Crossbody, Shoulder Bag, Handbags, Backpacks, Tote Bags');
+INSERT INTO Categories (category_name, description) VALUES ('Shoes', 'Flat Shoes, High-Heels, Sandals, Boots, Sneakers');
+INSERT INTO Categories (category_name, description) VALUES ('Accessories', 'Scarves, hats, gloves, Belts, Jewellery');
