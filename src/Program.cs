85441d39e6b6ced7b6934f4a5489d5dbd9ef00d0
Hawra al Ramadan <al.ramdan.hawra@gmail.com>
2024-05-02 14:11:57 +0300

    updating files

--- src/Program.cs
+++ src/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
