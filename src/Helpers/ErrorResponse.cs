4b302b8e7aa62eac26f0b52fc727552320a020d6
Hawra al Ramadan <al.ramdan.hawra@gmail.com>
2024-05-02 14:28:47 +0300

    Helpers

---
+++ src/Helpers/ErrorResponse.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace api.Controllers
+{
+  public class ErrorResponse
+  {
+    public bool Success { get; set; } = false;
+    public string? Message { get; set; }
+  }
+
+}
