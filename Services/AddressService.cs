03857f98d64cca48ecef0e569b21efdf3681720c
FatimalRamadan <2180004473@iau.edu.sa>
2024-05-12 02:36:13 +0300

    addressDto again

--- Services/AddressService.cs
+++ Services/AddressService.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Backend.EntityFramework;
-using Backend.Helpers;
 using Backend.Models;
+using Backend.Dtos;
 
 namespace Backend.Services
 {
@@ -13,9 +13,9 @@ namespace Backend.Services
             _appDbContext = appDbContext;
         }
 
-        public async Task<IEnumerable<Address>> GetAllAddressesAsync()
+        public async Task<IEnumerable<AddressDto>> GetAllAddressesAsync()
         {
-            return await _appDbContext.Addresses
+            return (IEnumerable<AddressDto>)await _appDbContext.Addresses
              .Select(p => new Address
              {
                  AddressId = p.AddressId,
@@ -28,31 +28,11 @@ namespace Backend.Services
              }).ToListAsync();
         }
 
-
         public async Task<Address?> GetAddressById(Guid addressId, Guid userId)
         {
             // display address with user create using include
-
-
             return await _appDbContext.Addresses.Include(address => address.User).FirstOrDefaultAsync(address => address.AddressId == addressId && address.UserId == userId);
         }
-
-        // get address by id with user auth 
-        // public async Task<Address?> GetAddressByIdWithUserAuth(Guid addressId)
-        // {
-        //     return await _appDbContext.Addresses
-        //      .Where(p => p.AddressId == addressId)
-        //      .Select(p => new Address
-        //       {
-        //           AddressId = p.AddressId,
-        //           AddressLine = p.AddressLine,
-        //           City = p.City,
-        //           State = p.State,
-        //           Country = p.Country,
-        //           ZipCode = p.ZipCode,
-        //           UserId = p.UserId
-        //       }).FirstOrDefaultAsync();
-        // }
 
          public async Task<Address> AddAddressService(Address newAddress, Guid userId)
         {
@@ -63,50 +43,24 @@ namespace Backend.Services
             return newAddress;
         }
 
-        // update address  by user  auth login
+        public async Task<Address?> UpdateAddressService(Guid addressId, Address updateAddress, Guid userId)
+        {
+            var existingAddress = await _appDbContext.Addresses.FirstOrDefaultAsync(a => a.AddressId == addressId && a.UserId == userId);
 
-   // Update address by user authentication
-public async Task<Address?> UpdateAddressService(Guid addressId, Address updateAddress, Guid userId)
-{
-    var existingAddress = await _appDbContext.Addresses.FirstOrDefaultAsync(a => a.AddressId == addressId && a.UserId == userId);
+            if (existingAddress != null)
+            {
+                existingAddress.AddressLine = updateAddress.AddressLine ?? existingAddress.AddressLine;
+                existingAddress.City = updateAddress.City ?? existingAddress.City;
+                existingAddress.State = updateAddress.State ?? existingAddress.State;
+                existingAddress.Country = updateAddress.Country ?? existingAddress.Country;
+                existingAddress.ZipCode = updateAddress.ZipCode ?? existingAddress.ZipCode;
 
-    if (existingAddress != null)
-    {
-        existingAddress.AddressLine = updateAddress.AddressLine ?? existingAddress.AddressLine;
-        existingAddress.City = updateAddress.City ?? existingAddress.City;
-        existingAddress.State = updateAddress.State ?? existingAddress.State;
-        existingAddress.Country = updateAddress.Country ?? existingAddress.Country;
-        existingAddress.ZipCode = updateAddress.ZipCode ?? existingAddress.ZipCode;
+                await _appDbContext.SaveChangesAsync();
+            }
 
-        await _appDbContext.SaveChangesAsync();
-    }
-
-    // Return the updated address (or null if not found or not belonging to the user)
-    return existingAddress;
-}
-        // public async Task<Address?> UpdateAddressService(Guid addressId, Address updateAddress)
-        // {
-        //     var existingAddress = await _appDbContext.Addresses.FirstOrDefaultAsync(a => a.AddressId == addressId);
-        //     if (existingAddress != null)
-        //     {
-        //         existingAddress.AddressLine = updateAddress.AddressLine ?? existingAddress.AddressLine;
-        //         existingAddress.City = updateAddress.City ?? existingAddress.City;
-        //         existingAddress.State = updateAddress.State ?? existingAddress.State;
-        //         existingAddress.Country = updateAddress.Country ?? existingAddress.Country;
-        //         existingAddress.ZipCode = updateAddress.ZipCode ?? existingAddress.ZipCode;
-        //         existingAddress.AddressLine = updateAddress.AddressLine ?? existingAddress.AddressLine;
-        //         existingAddress.City = updateAddress.City ?? existingAddress.City;
-        //         existingAddress.State = updateAddress.State ?? existingAddress.State;
-        //         existingAddress.Country = updateAddress.Country ?? existingAddress.Country;
-        //         existingAddress.ZipCode = updateAddress.ZipCode ?? existingAddress.ZipCode;
-        //         existingAddress.UserId = updateAddress.UserId;
-        //         await _appDbContext.SaveChangesAsync();
-        //     }
-        //     return existingAddress;
-        // }
-
-        
-
+            // Return the updated address (or null if not found or not belonging to the user)
+            return existingAddress;
+        } 
 
         public async Task<bool> DeleteAddressService(Guid addressId, Guid userId)
         {
